dcc6dacaf5d934c4db27fe3363c1fd6e3f06c3ce
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-04-28 01:24:03 +0530

    added card images

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 let cards = [];
 
-for(let i=1;i<10;i++){
-    for(let j=0;j<4;j++){
+for(let j=0;j<4;j++){
+for(let i=1;i<14;i++){
+    if(i>9){
+        cards.push(10);
+    }
+    else{
         cards.push(i);
     }
 }
-
-for(let i=0;i<16;i++){
-    cards.push(10);
 }
+
+console.log(cards);
+
+// QUERY SELECTORS
 
 const hitbtn = document.querySelector(".hitbtn");
 const stdbtn = document.querySelector(".stdbtn");
+
+const cards1 = document.querySelector(".player1");
+const cards2 = document.querySelector(".player2");
+const curr = document.querySelector(".curplayer");
 
 let count = 0;
 
@@ -38,6 +47,11 @@ function hit() {
     // remove that card from the array/deck
     cur_player.push(card);
 
+    // Updating the changes on screen
+
+    cards1.innerHTML = player1;
+    cards2.innerHTML = player2;
+
     // find the element's index and remove it from the array
 
     let index = 0;
@@ -46,6 +60,13 @@ function hit() {
             index = i;
         }
     }
+
+    // Adding card on the screen
+    var img = document.createElement("img");
+    let imgIndex = index+1;
+    console.log(imgIndex);
+    img.src = "./cards/"+imgIndex.toString()+".png";
+    cards1.appendChild(img);
 
     cards.splice(index, 1);
 
@@ -69,9 +90,11 @@ function hit() {
     console.log(cur_player);
 }
 
+
+
 function stand() {
+    curr.innerHTML = "PLAYER 2 PLAYS";
     console.log("STAND");
     cur_player = player2;
     // turn will move to the next player
 }
-let playing = true;
